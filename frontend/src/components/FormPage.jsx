577ca769f60d001a7f60542be2651f1b60411a2f
Kent-Danielle <kconcengco@my.bcit.ca>
2022-08-15 23:24:26 -0700

    Added a responsive progress bar and button on form page

--- frontend/src/components/FormPage.jsx
+++ frontend/src/components/FormPage.jsx
@@ -1,10 +1,30 @@
-import { Button, Box } from "@chakra-ui/react";
+import {
+	Button,
+	Box,
+	Flex,
+	VStack,
+	Heading,
+	Center,
+	ButtonGroup,
+} from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { CheckIcon } from "@chakra-ui/icons";
 
 function FormPage() {
+	const [step, setStep] = useState(0);
+
+	const handleNext = () => {
+		setStep(step + 1);
+	};
+
+	const handleBack = () => {
+		setStep(step - 1);
+	};
+
 	const navigate = useNavigate();
 	return (
-		<Box>
+		<>
 			<Button
 				opacity={0.85}
 				alignSelf={"flex-start"}
@@ -21,7 +41,69 @@ function FormPage() {
 			>
 				Return
 			</Button>
-		</Box>
+			<Flex justifyContent={"space-around"}>
+				<VStack w={"7em"}>
+					<Center
+						h={["2.5rem", "3rem"]}
+						w={["2.5rem", "3rem"]}
+						borderRadius={"full"}
+						bg={step >= 0 ? "carbon.400" : "gray.300"}
+						textAlign={"center"}
+						color={"white"}
+						fontWeight={"bold"}
+						fontSize={"xl"}
+					>
+						{step > 0 ? <CheckIcon /> : 1}
+					</Center>
+					<Heading as="h1" size="sm" textAlign={"center"}>
+						Yesterday's Order
+					</Heading>
+				</VStack>
+				<VStack w={"7em"}>
+					<Center
+						h={["2.5rem", "3rem"]}
+						w={["2.5rem", "3rem"]}
+						borderRadius={"full"}
+						bg={step >= 1 ? "carbon.400" : "gray.300"}
+						textAlign={"center"}
+						color={"white"}
+						fontWeight={"bold"}
+						fontSize={"xl"}
+					>
+						{step > 1 ? <CheckIcon /> : 2}
+					</Center>
+					f
+					<Heading as="h1" size="sm" textAlign={"center"}>
+						Sale Status
+					</Heading>
+				</VStack>
+				<VStack w={"7em"}>
+					<Center
+						h={["2.5rem", "3rem"]}
+						w={["2.5rem", "3rem"]}
+						borderRadius={"full"}
+						bg={step >= 2 ? "carbon.400" : "gray.300"}
+						textAlign={"center"}
+						color={"white"}
+						fontWeight={"bold"}
+						fontSize={"xl"}
+					>
+						{step > 2 ? <CheckIcon /> : 3}
+					</Center>
+					<Heading as="h1" size="sm" textAlign={"center"}>
+						Edit Order
+					</Heading>
+				</VStack>
+			</Flex>
+			<ButtonGroup justifySelf={"flex-end"} alignSelf={"center"} spacing="6">
+				<Button disabled={step === 0} onClick={handleBack}>
+					Back
+				</Button>
+				<Button disabled={step > 2} onClick={handleNext}>
+					{step === 2 ? "Submit" : "Next"}
+				</Button>
+			</ButtonGroup>
+		</>
 	);
 }
 
